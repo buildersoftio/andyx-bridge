2d6359f8fbfe9bfd65661b2ba56b8364d3ac8ff5
Enes Hoxha <eneshoxha@outlook.com>
2021-10-10 13:07:08 +0200

    update: app information

--- src/Andy.X.Bridge.Core/Utilities/Logging/Logger.cs
+++ src/Andy.X.Bridge.Core/Utilities/Logging/Logger.cs
@@ -41,6 +41,29 @@ namespace Andy.X.Bridge.Core.Utilities.Logging
             Trace.WriteLine(logWithRed);
             Console.ForegroundColor = generalColor;
         }
+
+        public static void ShowWelcomeTest()
+        {
+            var generalColor = Console.ForegroundColor;
+            Trace.WriteLine("                   Starting Buildersoft Andy X Bridge");
+            Trace.WriteLine("                   Copyright (C) 2021 Buildersoft LLC");
+            Trace.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Trace.Write("  ###"); Console.ForegroundColor = generalColor; Trace.WriteLine("      ###");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Trace.Write("    ###"); Console.ForegroundColor = generalColor; Trace.Write("  ###");
+            Trace.WriteLine("       Andy X Bridge 1.0.0-alpha. Copyright (C) 2021 Buildersoft LLC");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Trace.Write("      ####         "); Console.ForegroundColor = generalColor; Trace.WriteLine("Licensed under the Apache License 2.0.  See https://bit.ly/3DqVQbx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Trace.WriteLine("    ###  ###");
+            Trace.Write("  ###      ###     "); Console.ForegroundColor = generalColor; Trace.WriteLine("Andy X Bridge is an open-source distributed solution for integrating clusters of Andy X, Kafka, Pulsar and Message Queue Systems together");
+            Trace.WriteLine("");
+
+
+            Trace.WriteLine("                   Starting Buildersoft Andy X Bridge...");
+            Trace.WriteLine("\n");
+        }
     }
 
 }
